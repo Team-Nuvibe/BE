e7c7fa5445a73b747e58271a29ba6c7977037660
jaemin0413 <95339052+jaemin0413@users.noreply.github.com>
2026-01-04 18:27:46 +0900

    [SETTING] project init

---
+++ src/test/java/com/example/nuvibe/NuvibeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nuvibe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuvibeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
