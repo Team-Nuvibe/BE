2fa61b2f04b15f401126a5a23ebe21d656acda5e
jaemin0413 <95339052+jaemin0413@users.noreply.github.com>
2026-01-06 14:51:04 +0900

    [SETTING] 공통 응답 세팅

---
+++ src/main/java/com/umc/nuvibe/global/apiPayLoad/response/BaseResponse.java
@@ -0,0 +1,19 @@
+package com.umc.nuvibe.global.apiPayLoad.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class BaseResponse<T> {
+    private boolean success;
+    private String message;
+    private T data;
+
+    public static <T> BaseResponse<T> onSuccess(T data) {
+        return new BaseResponse<> (true, "success", data);
+    }
+
+}
