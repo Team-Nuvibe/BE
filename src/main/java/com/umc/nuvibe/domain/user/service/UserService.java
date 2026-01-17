bbc9c9a7c78629d22bf26374a2405112d1e7c157
jaemin0413 <95339052+jaemin0413@users.noreply.github.com>
2026-01-17 22:31:17 +0900

    [FEAT] 비밀번호 변경 기능 구현

--- src/main/java/com/umc/nuvibe/domain/user/service/UserService.java
+++ src/main/java/com/umc/nuvibe/domain/user/service/UserService.java
@@ -1,5 +1,6 @@
 package com.umc.nuvibe.domain.user.service;
 
+import com.umc.nuvibe.domain.user.dto.request.ReissuePasswordReq;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface UserService {
@@ -7,7 +8,7 @@ public interface UserService {
     void updateUserNickname(Long userId, String nickname);
     void requestEmailUpdate(Long userId, String email);
     void completeEmailUpdate(Long userId, String token);
-    void updatePassword(Long userId, String password);
+    void reissuePassword(Long userId, ReissuePasswordReq request);
     void updateSetting(Long userId);
 
 
--- src/main/java/com/umc/nuvibe/domain/user/service/UserServiceImpl.java
+++ src/main/java/com/umc/nuvibe/domain/user/service/UserServiceImpl.java
@@ -1,11 +1,10 @@
 package com.umc.nuvibe.domain.user.service;
 
 import com.umc.nuvibe.domain.image.service.ImageService;
+import com.umc.nuvibe.domain.user.dto.request.ReissuePasswordReq;
 import com.umc.nuvibe.domain.user.entity.User;
 import com.umc.nuvibe.domain.user.repository.UserRepository;
 import com.umc.nuvibe.global.apiPayLoad.error.AuthErrorCode;
-import com.umc.nuvibe.global.apiPayLoad.error.CommonErrorCode;
-import com.umc.nuvibe.global.apiPayLoad.error.MailErrorCode;
 import com.umc.nuvibe.global.apiPayLoad.error.UserErrorCode;
 import com.umc.nuvibe.global.apiPayLoad.exception.BusinessException;
 import com.umc.nuvibe.global.service.EmailVerificationService;
@@ -15,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
+import java.util.regex.Pattern;
 
 @Service
 @RequiredArgsConstructor
@@ -70,8 +70,12 @@ public class UserServiceImpl implements UserService {
 
     @Override
     @Transactional
-    public void updatePassword(Long userId, String password) {
+    public void reissuePassword(Long userId, ReissuePasswordReq request) {
+        validateReissuePassword(request);
+        User user=userRepository.findById(userId)
+                .orElseThrow(()-> new BusinessException(UserErrorCode.USER_NOT_FOUND));
 
+        user.updatePassword(request.password());
     }
 
     @Override
@@ -79,4 +83,17 @@ public class UserServiceImpl implements UserService {
     public void updateSetting(Long userId) {
 
     }
+
+    private void validateReissuePassword(ReissuePasswordReq request) {
+        if (!PASSWORD_PATTERN.matcher(request.password()).matches()) {
+            throw new BusinessException(AuthErrorCode.INVALID_PASSWORD_FORMAT);
+        }
+
+        if(!request.password().equals(request.confirmPassword())) {
+            throw new BusinessException(AuthErrorCode.CONFIRM_PASSWORD_MISMATCH);
+        }
+    }
+
+    private static final Pattern PASSWORD_PATTERN =
+            Pattern.compile("^(?=.*[a-zA-Z])(?=.*\\d)(?=.*\\W).{8,20}$");
 }
