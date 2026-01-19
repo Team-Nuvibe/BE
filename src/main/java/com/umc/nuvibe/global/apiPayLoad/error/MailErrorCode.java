c166345080024684b0636a6fbf06e73ea4455d47
jaemin0413 <95339052+jaemin0413@users.noreply.github.com>
2026-01-19 21:42:13 +0900

    [REFACTOR] 코드리뷰 반영(토큰 해싱, 평문 로그 삭제)

--- src/main/java/com/umc/nuvibe/global/apiPayLoad/error/MailErrorCode.java
+++ src/main/java/com/umc/nuvibe/global/apiPayLoad/error/MailErrorCode.java
@@ -12,6 +12,7 @@ public enum MailErrorCode implements ErrorCode {
     TOKEN_EXPIRED(HttpStatus.BAD_REQUEST,"MAIL002","만료된 토큰입니다."),
     EMAIL_NOT_VERIFIED(HttpStatus.BAD_REQUEST,"MAIL003","인증되지 않은 이메일입니다."),
     EMAIL_SEND_FAILED(HttpStatus.INTERNAL_SERVER_ERROR,"MAIL004","이메일 발송에 실패했습니다."),
+    NO_SHA_256(HttpStatus.BAD_REQUEST, "MAIL005","sha-256 알고리즘이 없습니다"),
 
     ;
 
