b403fbb5743f25c30f40e2095b9b0dff2f2f20b4
이창훈 <ckzlsn87@gmail.com>
2026-01-27 21:26:39 +0900

    [REFACTOR] #54:@Param 추가

--- src/main/java/com/umc/nuvibe/domain/user/repository/UserRepository.java
+++ src/main/java/com/umc/nuvibe/domain/user/repository/UserRepository.java
@@ -6,10 +6,8 @@ import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -26,7 +24,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
         WHERE bi.createdAt >= :todayStart
     )
     """)
-    Slice<User> findUsersWithoutDropToday(LocalDateTime todayStart, Pageable pageable);
+    Slice<User> findUsersWithoutDropToday(@Param("todayStart") LocalDateTime todayStart, Pageable pageable);
 
     // 특정 날짜 이후 드랍 없는 사용자 조회
     @Query("""
