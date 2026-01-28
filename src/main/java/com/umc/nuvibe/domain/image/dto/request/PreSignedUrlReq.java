8807d174be9fe7dba574b538a98ecf30e7cf6c0c
jaemin0413 <95339052+jaemin0413@users.noreply.github.com>
2026-01-28 21:23:15 +0900

    [FEAT] presigned Url 발급 로직 구현

---
+++ src/main/java/com/umc/nuvibe/domain/image/dto/request/PreSignedUrlReq.java
@@ -0,0 +1,6 @@
+package com.umc.nuvibe.domain.image.dto.request;
+
+public record PreSignedUrlReq(
+        String originalFileName
+) {
+}
---
+++ src/main/java/com/umc/nuvibe/domain/image/dto/response/PreSignedUrlRes.java
@@ -0,0 +1,7 @@
+package com.umc.nuvibe.domain.image.dto.response;
+
+public record PreSignedUrlRes (
+        String url,
+        String fileName
+){
+}
