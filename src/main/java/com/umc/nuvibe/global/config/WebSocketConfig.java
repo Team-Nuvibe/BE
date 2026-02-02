e8067b71ead78fe244fbb60b4302532e84ba762c
ryun <ryun0038@gmail.com>
2026-02-02 23:40:36 +0900

    [FEAT] #91: 웹소켓 테스크 스케줄러 추가

--- src/main/java/com/umc/nuvibe/global/config/WebSocketConfig.java
+++ src/main/java/com/umc/nuvibe/global/config/WebSocketConfig.java
@@ -2,9 +2,12 @@ package com.umc.nuvibe.global.config;
 
 import com.umc.nuvibe.global.security.websocket.StompAuthChannelInterceptor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -29,7 +32,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         // /topic으로 시작하는 destination 처리 및 프록시 타임아웃 방지
         registry.enableSimpleBroker("/topic")
-                .setHeartbeatValue(new long[]{10000, 10000});
+                .setHeartbeatValue(new long[]{10000, 10000})
+                .setTaskScheduler(taskScheduler());;
         // STOMP 표준 구조 위해 유지
         registry.setApplicationDestinationPrefixes("/app");
     }
@@ -37,5 +41,14 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureClientInboundChannel(ChannelRegistration registration) {
         registration.interceptors(stompAuthChannelInterceptor);
+    }
+
+    @Bean
+    public TaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(1);
+        scheduler.setThreadNamePrefix("websocket-heartbeat-");
+        scheduler.initialize();
+        return scheduler;
     }
 }
