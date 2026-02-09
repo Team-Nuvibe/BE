2d02fb561a526d0394216afd4aee5982d0d81f44
ryun <ryun0038@gmail.com>
2026-02-09 12:57:39 +0900

    [REFACTOR]#102: 스크랩 dto 반환값 수정

--- src/main/java/com/umc/nuvibe/domain/tribe/dto/response/scrapedImage/ScrapedImageItemRes.java
+++ src/main/java/com/umc/nuvibe/domain/tribe/dto/response/scrapedImage/ScrapedImageItemRes.java
@@ -26,7 +26,7 @@ public record ScrapedImageItemRes(
         @Schema(description = "채팅 ID")
         Long chatId,
 
-        @Schema(description = "채팅 작성자 ID")
-        Long chatSenderId
+        @Schema(description = "채팅 작성자 닉네임")
+        String chatSenderNickname
 ) {
 }
